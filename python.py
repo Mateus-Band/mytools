92ed7a7c74b242f0d6e4b60304f17628b721cf02
Mateus-Band <al.mateus.torres@impatech.edu.br>
2025-08-11 15:03:08 -0300

    Add files via upload

---
+++ python.py
@@ -0,0 +1,9 @@
+from MYTOOLS import pi_real, e_real, PI_INT, E_INT
+
+x = int(input("Enter the number of decimal places for π: "))
+y = int(input("Enter the number of decimal places for e: "))
+print(f"\nπ to {x} decimal places: {pi_real(x)}")
+print(f"e to {y} decimal places: {e_real(y)}")
+print(f"\nFull π integer part: 3,{PI_INT}")
+print(f"\nFull e integer part: 2,{E_INT}")
+
